72e286383895b8a273b08cd5d449d5432c0d5d04
Lad Jankovic <ljankovic@yahoo.com>
2022-04-23 11:55:14 -0400

    State used for Tweets

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import Tweet from "./Tweet";
 
 function App(){
 
-  const [isRed, setRed] = useState(false);
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-    setRed(!isRed);
-  }
+  const [users, setUsers] = useState([
+    {name: 'Ed', message: 'Hello'},
+    {name: 'John', message: 'Im John'},
+    {name: 'Trav', message: 'Yeah, yeah' } 
+  ]);
 
   return(
     <div className="App">
-      <h1 className={isRed ? 'red': '' }>Change my color</h1>
-      <button onClick={increment}>Increment</button>
-      <h1>{count}</h1>
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message}/>
+      ))}
     </div>
   );
 }
