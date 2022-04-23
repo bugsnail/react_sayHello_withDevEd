252935e8ed962598d1e14cc0806a65fbbc1db575
Lad Jankovic <ljankovic@yahoo.com>
2022-04-23 10:32:53 -0400

    Say Hello component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Hello from "./sayHello";
+
+function App(){
+
+  return(
+    <div>This is the app component
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
